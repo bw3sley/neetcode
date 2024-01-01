79f15ae00dd8cbaa9ea741ad601df8f02517970e
Wesley Bernardes <bw3sley@gmail.com>
2024-01-01 19:51:20 -0300

    :tada: chore: solve contains duplicate challenge

---
+++ arrays-hashing/contains-duplicate/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+*/
+
+function containsDuplicate(nums) {
+    const seen = new Set();
+
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+
+        seen.add(num);
+    }
+
+    return false;
+}
